c942839c51a54d248351bae263751ccdaf3b37a6
Anastasiia Yermakova <yermak1037@gmail.com>
2020-09-17 22:12:29 +0300

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -1,9 +1,12 @@
 const inputEl = document.getElementById('validation-input');
 
-const makeCheckValidation = valueInput => {
-  if (valueInput.target.value.length === Number(inputEl.dataset.length)) {
+const makeCheckValidation = event => {
+  if (event.target.value.length === Number(inputEl.dataset.length)) {
     inputEl.classList.add('valid');
     inputEl.classList.remove('invalid');
+  } else if (event.target.value.length === 0) {
+    inputEl.classList.remove('invalid');
+    inputEl.classList.remove('valid');
   } else {
     inputEl.classList.add('invalid');
     inputEl.classList.remove('valid');
